2efaba06c9e6731633cbb14fdd7fa4033941fbea
Marlon Henry <haveyec@me.com>
2023-12-30 13:43:28 -0500

    added categories and courses

---
+++ db/migrate/20231230184203_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.string :subtitle
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
