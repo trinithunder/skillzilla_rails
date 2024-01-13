304c880142b436efc109e7b8b17bdec9fe939e7a
Marlon Henry <haveyec@me.com>
2024-01-12 19:22:36 -0500

    Changed db, blog in place, next some design changes need to be made

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,3 @@
 class Quiz < ApplicationRecord
+  has_one_attached :quiz_video
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_08_212434) do
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_212434) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,114 +33,114 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_212434) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "blogs", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "courses", force: :cascade do |t|
+  create_table "courses", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_courses_on_category_id"
   end
 
-  create_table "days_streaks", force: :cascade do |t|
+  create_table "days_streaks", charset: "utf8", force: :cascade do |t|
     t.integer "count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "faqs", force: :cascade do |t|
+  create_table "faqs", charset: "utf8", force: :cascade do |t|
     t.string "question"
     t.text "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "homes", force: :cascade do |t|
+  create_table "homes", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "hubs", force: :cascade do |t|
+  create_table "hubs", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "learning_styles", force: :cascade do |t|
+  create_table "learning_styles", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "leaves", force: :cascade do |t|
+  create_table "leaves", charset: "utf8", force: :cascade do |t|
     t.integer "count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "life_stages", force: :cascade do |t|
+  create_table "life_stages", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "notifications", force: :cascade do |t|
+  create_table "notifications", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "onboardings", force: :cascade do |t|
+  create_table "onboardings", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", charset: "utf8", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "privacy_policies", force: :cascade do |t|
+  create_table "privacy_policies", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "quizzes", force: :cascade do |t|
+  create_table "quizzes", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "subscriptions", force: :cascade do |t|
+  create_table "subscriptions", charset: "utf8", force: :cascade do |t|
     t.string "plan_name"
     t.integer "plan_price"
     t.date "plan_billing_date"
@@ -148,31 +148,31 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_212434) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "terms_of_services", force: :cascade do |t|
+  create_table "terms_of_services", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "topic_comments", force: :cascade do |t|
+  create_table "topic_comments", charset: "utf8", force: :cascade do |t|
     t.text "body"
-    t.integer "topic_id", null: false
+    t.bigint "topic_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_topic_comments_on_topic_id"
   end
 
-  create_table "topics", force: :cascade do |t|
+  create_table "topics", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_topics_on_category_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -181,7 +181,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_212434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "benefit_status"
-    t.integer "subscription_id", null: false
+    t.bigint "subscription_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["subscription_id"], name: "index_users_on_subscription_id"
